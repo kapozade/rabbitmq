aab2f693beedd54e2360df3aba88ed903f8bc642
Onur <onurkapcik@gmail.com>
2023-06-02 16:36:52 +0300

    Add second consumer

---
+++ WorkQueue/SecondConsumer/src/WorkQueue.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using WorkQueue.Infrastructure;
+
+var hostBuilder = CreateDefaultBuilder();
+var host = hostBuilder.Build();
+host.Run();
+
+static IHostBuilder CreateDefaultBuilder()
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(app =>
+        {
+            app.SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        }).ConfigureServices((context, serviceCollection) =>
+        {
+            CompositionRoot.AddDependencies(serviceCollection, context.Configuration);
+        });
+}
---
+++ WorkQueue/SecondConsumer/src/WorkQueue.Infrastructure/Messaging/FakeDataQueueConsumer.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+using WorkQueue.Core.FakeData;
+using WorkQueue.Core.Messaging;
+using WorkQueue.Core.Messaging.Settings;
+
+namespace WorkQueue.Infrastructure.Messaging;
+
+public class FakeDataQueueConsumer : BaseQueueConsumer<FakeData>, IFakeDataQueueConsumer
+{
+    protected override string QueueName => "q.work-queue.example";
+    protected override bool AutoAck => false; // Message is acked after processed.
+
+    public FakeDataQueueConsumer(
+            RabbitMqSettings settings, 
+            ILogger<FakeDataQueueConsumer> logger
+        ) : base(settings, logger)
+    {
+        GenerateChannel();
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+    }
+}
