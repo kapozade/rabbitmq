6d05de176bc8923d783005b16d468dc3f8d991f6
Onur <onurkapcik@gmail.com>
2023-06-03 16:53:46 +0300

    Add BaseProducerQueue.

--- Topic/Producer/src/Topic.Core/Messaging/Settings/RabbitMqSettings.cs
+++ Topic/Producer/src/Topic.Core/Messaging/Settings/RabbitMqSettings.cs
@@ -2,7 +2,7 @@ namespace Topic.Core.Messaging.Settings;
 
 public sealed record RabbitMqSettings
 {
-    public string UserName { get; init; } = string.Empty;
+    public string Username { get; init; } = string.Empty;
     public string Password { get; init; } = string.Empty;
     public string Host { get; init; } = string.Empty;
     public string VirtualHost { get; init; } = string.Empty;
