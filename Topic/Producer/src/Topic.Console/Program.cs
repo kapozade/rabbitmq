56d7eb4278e3951aba6a647857ddf0a1953673ff
Onur <onurkapcik@gmail.com>
2023-06-03 16:34:22 +0300

    Add infrastructure layer.

--- Topic/Producer/src/Topic.Console/Program.cs
+++ Topic/Producer/src/Topic.Console/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Topic.Infrastructure;
+
+var hostBuilder = CreateHostBuilder();
+var host = hostBuilder.Build();
+host.Run();
+
+static IHostBuilder CreateHostBuilder()
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(cfg =>
+        {
+            cfg.SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        })
+        .ConfigureServices((ctx, collection) =>
+        {
+            CompositionRoot.AddDependencies(collection, ctx.Configuration);
+        });
+}
---
+++ Topic/Producer/src/Topic.Infrastructure/CompositionRoot.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Topic.Infrastructure;
+
+public static class CompositionRoot
+{
+    public static void AddDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+    }
+}
