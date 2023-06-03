b2886221cd95000912a612e01006edca35c8d74b
Onur <onurkapcik@gmail.com>
2023-06-03 13:06:54 +0300

    Implement Topic Consumer

---
+++ Topic/Consumer/src/Topic.Infrastructure/CompositionRoot.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Topic.Application.BackgroundServices;
+using Topic.Core.Messaging;
+using Topic.Core.Messaging.Settings;
+using Topic.Infrastructure.Messaging;
+
+namespace Topic.Infrastructure;
+
+public static class CompositionRoot
+{
+    public static void AddDependencies(IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        var rabbitMqSettings = configuration.GetSection("RabbitMqSettings").Get<RabbitMqSettings>()
+                               ?? throw new UnreachableException("RabbitMqSettings is not configured properly.");
+        serviceCollection.AddSingleton(rabbitMqSettings);
+
+        serviceCollection.AddSingleton<IFakeDataConsumerQueue, Topic1ConsumerQueue>();
+        serviceCollection.AddHostedService<Topic1ConsumerQueueBackgroundService>();
+    }
+}
---
+++ Topic/Consumer/src/Topic.Infrastructure/Messaging/Topic1ConsumerQueue.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Logging;
+using Topic.Core.Data;
+using Topic.Core.Messaging;
+using Topic.Core.Messaging.Settings;
+
+namespace Topic.Infrastructure.Messaging;
+
+public sealed class Topic1ConsumerQueue : BaseQueueConsumer<FakeData>, IFakeDataConsumerQueue
+{
+    protected override string ExchangeName => "ex.topic.example";
+    protected override string QueueName => "q.topic.example-1";
+    protected override string RoutingKey => "topic-1";
+
+    public Topic1ConsumerQueue(
+        RabbitMqSettings settings, 
+        ILogger<Topic1ConsumerQueue> logger
+        ) : base(settings, logger)
+    {
+        GenerateChannel();
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+    }
+}
