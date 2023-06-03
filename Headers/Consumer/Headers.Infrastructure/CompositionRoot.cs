7365c3a04e478c7f2378889500bbbd94e4910335
Onur <onurkapcik@gmail.com>
2023-06-03 17:50:47 +0300

    Add infrastructure layer.

---
+++ Headers/Consumer/Headers.Infrastructure/CompositionRoot.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Headers.Infrastructure;
+
+public static class CompositionRoot
+{
+    public static void AddDependencies(IServiceCollection serviceCollection, 
+        IConfiguration configuration)
+    {
+        
+    }
+}
