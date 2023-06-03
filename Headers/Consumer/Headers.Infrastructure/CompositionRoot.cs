45792abf80acc7c9626aed4d1b0254fb006e5055
Onur <onurkapcik@gmail.com>
2023-06-03 18:21:05 +0300

    Impelement queue consumers.

--- Headers/Consumer/Headers.Infrastructure/CompositionRoot.cs
+++ Headers/Consumer/Headers.Infrastructure/CompositionRoot.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics;
+using Headers.Core.Messaging;
+using Headers.Core.Messaging.Settings;
+using Headers.Infrastructure.Messaging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Headers.Infrastructure;
@@ -8,6 +12,11 @@ public static class CompositionRoot
     public static void AddDependencies(IServiceCollection serviceCollection, 
         IConfiguration configuration)
     {
-        
+        var rabbitMqSettings = configuration.GetSection("RabbitMqSettings").Get<RabbitMqSettings>()
+                               ?? throw new UnreachableException("RabbitMqSettings is not configured properly.");
+        serviceCollection.AddSingleton(rabbitMqSettings);
+
+        serviceCollection.AddSingleton<IHeader1ConsumerQueue, Header1ConsumerQueue>();
+        serviceCollection.AddSingleton<IHeader2ConsumerQueue, Header2ConsumerQueue>();
     }
 }
